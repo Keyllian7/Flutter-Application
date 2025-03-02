bb06870925d36d555838376811fc47297f9bf671
Keyllian7 <keyllianazevedo2@gmail.com>
2025-03-02 20:14:49 -0300

    feat: learning better about states

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp(title: 'Aprendendo Flutter'));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String title;
 
+  const MyApp({super.key, required this.title});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
-      home: const MyHomePage(title: 'Estudando Flutter'),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomePageState extends State<HomePage> {
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Contagem:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text('Teste de colunas')
-          ],
+    return Container(
+      child: Center(
+        child: GestureDetector(
+          child: Text(
+            'Contagem: $counter',
+            style: TextStyle(color: Colors.blue),
+          ),
+          onTap: () {
+            setState(() {
+              counter++;
+              print('$counter');
+            });
+          },
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }

35a038100a9f0590104fd38341a141f3d25511a1
Keyllian7 <keyllianazevedo2@gmail.com>
2025-03-06 23:47:15 -0300

    refactor: componentizing widgets on the challenge screen

--- lib/challenge_page.dart
+++ lib/challenge_page.dart
@@ -14,37 +14,70 @@ class Challenge extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 150,
-              child: Image.asset('assets/images/tinder.png'),
-            ),
+            ImageLogo(),
             SizedBox(height: 50),
-            Text(
-              'Location Changer',
-              style: TextStyle(color: Colors.white, fontSize: 30),
-            ),
+            Title(),
             SizedBox(height: 5),
-            Text(
-              'Plugin app for tinder',
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
+            Subtitle(),
             SizedBox(height: 50),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.redAccent,
-                minimumSize: Size(350, 75),
-              ),
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed('/home');
-              },
-              child: Text(
-                'Login with Facebook',
-                style: TextStyle(fontSize: 25),
-              ),
-            ),
+            Button(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ImageLogo extends StatelessWidget {
+  const ImageLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Image.asset('assets/images/tinder.png'),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Location Changer',
+      style: TextStyle(color: Colors.white, fontSize: 30),
+    );
+  }
+}
+
+class Subtitle extends StatelessWidget {
+  const Subtitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Plugin app for tinder',
+      style: TextStyle(color: Colors.white, fontSize: 20),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  const Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.redAccent,
+        minimumSize: Size(350, 75),
+      ),
+      onPressed: () {
+        Navigator.of(context).pushReplacementNamed('/home');
+      },
+      child: Text('Login with Facebook', style: TextStyle(fontSize: 25)),
     );
   }
 }
